5ca8a3198e2dc1034b1f145b6b1b1dd642fe1adc
John Lee <2003jjl@gmail.com>
2024-09-26 13:58:13 -0500

    Pushing refactored Landing with text

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,10 +5,10 @@ export const Landing = () => {
   return (
     <div className="landing-container">
       <div className="landing-content">
-        <img src="../assets/kowalski.jpg" alt="Headshot" className="headshot" />
+        <img src="src/assets/kowalski.png" alt="Headshot" className="headshot" />
         <div className="landing-text">
           <h1>Your Name</h1>
-          <p>Web Developer & Designer</p>
+          <p>Student and Penguin</p>
           <p>Welcome to my portfolio!</p>
         </div>
       </div>
