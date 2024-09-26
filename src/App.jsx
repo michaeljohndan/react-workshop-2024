48800e9c9953ccc601a47a8176134f070be2bc5b
steve <stevejung394@gmail.com>
2024-09-26 12:41:09 -0500

    componentize cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 
 import { Navbar } from './components/Navbar'
+import { ProjectList } from './components/ProjectList'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <ProjectList />
     </>
   )
 }
