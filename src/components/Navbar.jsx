71b8134429d29d0677d24c379dd0278fcd2cf3c0
John Lee <2003jjl@gmail.com>
2024-09-26 10:24:59 -0500

    First push

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <header className='header'>
+      
+      <a href="/" className="logo">Logo</a>
+      
+      <nav className="navbar"></nav>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Portfolio</a>
+        <a href="/">Services</a>
+        <a href="/">Contact</a>
+    </header>
+  )
+}
